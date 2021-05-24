bf662b122b2c74d96da2a54c20e763c4e10c7bb5
Safal Thapaliya <safalthapaliya@gmail.com>
2021-05-24 14:09:22 +0545

    starwars

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  let arnold = document.querySelector(`#arnold`);
+  let arnold = document.querySelector(`#obiwan`);
   arnold.addEventListener("click", (event) => {
     if (event.detail === 3) {
       let heading = document.querySelector("h1");
       let changeme = document.querySelector(`#changeme`);
-      changeme.innerText = `Who are you?`;
-      heading.innerText = "Why are you still here?";
+      changeme.innerText = `Oh wait....What is he doing?`;
+      heading.innerText = "Seems like you have found him.";
       const parent = arnold.parentElement;
       parent.removeChild(arnold);
       let newImage = document.createElement("img");
-      newImage.src = "./img/click.webp";
-      newImage.alt = "You should go now.";
+      newImage.src = "./img/anakin.webp";
+      newImage.alt = "Anakin Kill";
       parent.appendChild(newImage);
     }
   });
